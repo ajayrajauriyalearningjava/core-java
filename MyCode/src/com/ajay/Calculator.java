545273f2f04a39268c5ae575e07ecd3dec8d79f6
sony <sony@sony-VAIO>
2018-07-03 01:13:03 +0530

    Adding This Test Calculator Example into repo.

---
+++ MyCode/src/com/ajay/Calculator.java
@@ -0,0 +1,15 @@
+package com.ajay;
+
+public class Calculator {
+	public int add(int num1, int num2) {
+
+		int sum = num1 + num2;
+		return sum;
+	}
+
+	public int sub(int num1, int num2) {
+
+		int subtract = num1 + num2;
+		return subtract;
+	}
+}
---
+++ MyCode/src/com/ajay/TestCalculator.java
@@ -0,0 +1,12 @@
+package com.ajay;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		System.out.println(c.add(100, 200));
+		System.out.println(c.sub(200, 100));
+
+	}
+
+}
